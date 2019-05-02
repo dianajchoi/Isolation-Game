92a07f8f52699f7c3b88f63f2b23c4460fe441cb
dianajchoi <dianajchoi@gmail.com>
2019-05-01 19:41:56 -0700

    added first skeleton class

---
+++ CS4200-Program3/src/Minimax.java
@@ -0,0 +1,21 @@
+
+public class Minimax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void drawBoard() {
+		// TODO
+	}
+	
+	public void findNextMove() {
+		// TODO
+	}
+	
+	public void inputOpponentMove(String location) {
+		// TODO
+	}
+
+}
